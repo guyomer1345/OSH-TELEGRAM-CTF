35e32e395be676f78900af5ac77e22998b499370
Guy <guyo0503@gmail.com>
2022-03-29 00:48:17 +0300

    level1 encode error

--- level1/level1_create.py
+++ level1/level1_create.py
@@ -4,6 +4,7 @@ import os
 
 logging.basicConfig(level=logging.INFO)
 
+PADDING = "False"
 BASE_DIR = os.getcwd()
 LEVEL_FILE = os.path.join(BASE_DIR, 'Level1.txt')
 BINARY_HEADER = 2
@@ -22,7 +23,7 @@ def main(text):
     encrypted_message = encrypt_binary(binary_text)
 
     with open(LEVEL_FILE, 'w') as f:
-        f.write(encrypted_message)
+        f.write(PADDING  + encrypted_message)
 
     logging.info(f'Successfuly wrote Level 1 in {LEVEL_FILE}')
 
